9271f6a8f3b6e06cc792893c5d0274f4187a0099
Robbie Biesser <rbiesser@gmail.com>
2020-07-06 23:54:02 -0400

    refactor with added components

--- public/app/app.component.ts
+++ public/app/app.component.ts
@@ -1,7 +1,13 @@
 import { Component } from '@angular/core';
+// added AuthenticationService
+import { AuthenticationService } from './authentication/authentication.service';
+// added Router
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'mean-app',
   template: '<h1>Hello World</h1>',
 })
-export class AppComponent {}
+export class AppComponent {
+  constructor(private _authenticationService: AuthenticationService, private router: Router) { }
+}
--- public/app/app.module.ts
+++ public/app/app.module.ts
@@ -1,14 +1,34 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+// added FormsModule
+import { FormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
+// added RequestOptions
+import { HttpModule, RequestOptions } from '@angular/http';
+// added LocationStrategy, HashLocationStrategy
+import { LocationStrategy, HashLocationStrategy} from '@angular/common';
 
 import { AppComponent } from './app.component';
+import { AppRoutes } from './app.routes';
+
+import { HomeModule } from './home/home.module';
+import { AuthenticationService } from './authentication/authentication.service';
+import { AuthenticationModule } from './authentication/authentication.module';
 
 @NgModule({
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpModule,
+    FormsModule,
+    AuthenticationModule,
+    HomeModule,
+    RouterModule.forRoot(AppRoutes),
   ],
   declarations: [
     AppComponent
+  ],
+  providers: [
+    AuthenticationService
   ],
   bootstrap: [AppComponent]
 })
