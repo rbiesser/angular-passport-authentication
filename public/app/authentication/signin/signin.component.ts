736e622cd63bfa2c0f7dc4345d5c1995982fa5cb
Robbie Biesser <rbiesser@gmail.com>
2020-07-07 17:32:33 -0400

    fix formatting issue

--- public/app/authentication/signin/signin.component.ts
+++ public/app/authentication/signin/signin.component.ts
@@ -11,12 +11,12 @@ export class SigninComponent {
   errorMessage: string;
   credentials: any = {};
 
-  constructor(private _authenticationService: AuthenticationService
-    , private _router: Router) { }
+  constructor(private _authenticationService: AuthenticationService, private _router: Router) {
+
+  }
 
   signin() {
-    this._authenticationService.signin(this.credentials)
-      .subscribe(result => this._router.navigate(['/']),
-        error => this.errorMessage = error);
+    this._authenticationService.signin(this.credentials).subscribe(result => this._router.navigate(['/']),
+      error => this.errorMessage = error);
   }
 }
